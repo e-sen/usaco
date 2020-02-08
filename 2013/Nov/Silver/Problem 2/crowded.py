cd3c789c220d00049c70e914544849f1dd74346c
Jiahua.Zhang <i@iamee.me>
2020-02-08 19:48:40 +0800

    试试看使用一个数组能不能把事情搞定，不重新创建数组了

--- 2013/Nov/Silver/Problem 2/crowded.py
+++ 2013/Nov/Silver/Problem 2/crowded.py
@@ -1,15 +1,15 @@
 with open('crowded.in') as fw:
     sample = fw.read()
-    lines = map(lambda x: x.split(' '), sample.split('\n'))
-    N = int(lines[0][0])
-    D = int(lines[0][1])
-
-
+    lines = map(lambda x: map(lambda y: int(y), x.split(' ')), sample.split('\n'))
+    N = lines[0][0]
+    D = lines[0][1]
+    lineSlice = slice(1, 1 + N)
+    cows = sorted(lines[lineSlice], key=lambda x: x[0])
 
     # 2..1+N
-    for i in range(1, 1 + N):
-        print i
-        xi = lines[i][0]
-        hi = lines[i][1]
+    for cow in cows:
+        xi = cow[0]
+        hi = cow[1]
+        print "xi:", xi, "hi:", hi
 
     # input format finished.
